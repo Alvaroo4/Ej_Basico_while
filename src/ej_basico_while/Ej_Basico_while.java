61989eb4c05102526e11823d17a6c4b0addd9105
Alvaro <Alvaro@LAPTOP-H2RELL5F>
2021-03-15 19:20:50 +0100

    se ha modificado una palabra

---
+++ src/ej_basico_while/Ej_Basico_while.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej_basico_while;
+
+/**
+ *
+ * @author Alvaro
+ */
+public class Ej_Basico_while {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i;
+        i=0;
+        while(i<=100){
+            System.out.print(i+ " ");
+            i++;
+        }
+        // TODO code application logic here
+    }
+    
+}
